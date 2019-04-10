b7cb8e0f5be272452d86c74a7215ccc5e03c363f
Xaalan <lucie.muller-71@hotmail.com>
2019-04-10 09:33:57 +0200

    Finish DataFrame

--- src/main/java/devops/project/DataFrame.java
+++ src/main/java/devops/project/DataFrame.java
@@ -2,20 +2,21 @@ package devops.project;
 
 import java.util.ArrayList;
 import java.io.*;
+import java.util.Scanner;
 
 public class DataFrame{
 
 	private ArrayList<String> labels;
-	private ArrayList<ArrayList<?>> df;
+	private ArrayList<ArrayList<Object>> df;
 	
 	
 	public DataFrame() {
-		df = new ArrayList<ArrayList<?>>();
+		df = new ArrayList<ArrayList<Object>>();
 		labels = new ArrayList<String>();
 	}
 	
 	public DataFrame(Object[][] initArray) {
-		df = new ArrayList<ArrayList<?>>();
+		df = new ArrayList<ArrayList<Object>>();
 		
 		// Init labels
 		labels = new ArrayList<String>();
@@ -33,11 +34,39 @@ public class DataFrame{
 		}
 	}
 	
+	@SuppressWarnings("resource")
 	public DataFrame(String csv) {
+		df = new ArrayList<ArrayList<Object>>();
 		File file = new File(csv);
+		Scanner scFile, scString;
+		try {
+			scFile = new Scanner(file);
+			
+			//labels
+			labels = new ArrayList<String>();
+			scString = new Scanner(scFile.nextLine()).useDelimiter("\\,");
+			while (scString.hasNext()) {
+				labels.add(scString.next());
+			}
+			scString.close();
+			
+			//data
+			while (scFile.hasNextLine()) {
+				ArrayList<Object> newRow = new ArrayList<Object>();
+				scString = new Scanner(scFile.nextLine()).useDelimiter("\\,");
+				while (scString.hasNext()) {
+					newRow.add(scString.next());
+				}
+				df.add(newRow);
+				scString.close();
+			}
+			scFile.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public void addCol(ArrayList<?> col) {
+	public void addCol(ArrayList<Object> col) {
 		df.add(col);	
 	}
 	
@@ -45,4 +74,25 @@ public class DataFrame{
 		return df.get(col);
 	}
 	
+	public String toString() {
+		String ret = "";
+		
+		//labels
+		for (int i = 0; i < labels.size() - 1; i++) {
+			ret = ret + labels.get(i) + ",";
+		}
+		if (labels.size() != 0)
+			ret = ret + labels.get(labels.size() - 1) + "\n";
+		
+		//data
+		for (ArrayList<Object> row : df) {
+			for (int i = 0; i < row.size() - 1; i++) {
+				ret = ret + row.get(i) + ",";
+			}
+			if (row.size() != 0)
+				ret = ret + row.get(row.size() - 1) + "\n";
+		}
+		
+		return ret;
+	}
 }
